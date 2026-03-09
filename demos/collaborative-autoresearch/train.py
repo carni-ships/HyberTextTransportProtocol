0b7e109856a5788c77f81498a6de15761489ab2f
HyberText <hybertext@berachain.com>
2026-03-09 19:17:06 -0400

    autoresearch/mar9-s: seq_len=96 on MPS sweep

--- demos/collaborative-autoresearch/train.py
+++ demos/collaborative-autoresearch/train.py
@@ -25,7 +25,7 @@ from prepare import (
 @dataclass
 class GPTConfig:
     vocab_size:   int = VOCAB_SIZE
-    sequence_len: int = 80        # testing on MPS: can afford longer context given 2x throughput
+    sequence_len: int = 96        # testing on MPS: extended sweep
     n_layer:      int = 1
     n_head:       int = 4
     n_embd:       int = 128
